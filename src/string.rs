558db90f32ad02489c5d70ea425377c06f4352fa
Njagi Mwaniki <njagi@urbanslug.com>
2022-05-12 12:28:02 +0200

    Add some string conversion functions

--- src/string.rs
+++ src/string.rs
@@ -34,6 +34,54 @@ pub fn u8_slice_to_u32(v: &[u8]) -> u32 {
     })
 }
 
+// rename to ASCII
+fn unsigned_literal_to_u8<T: num::Unsigned + num::ToPrimitive>(v: T) -> u8 {
+    v.to_u8().unwrap() + ASCII_ZERO
+}
+
+// is this a good idea?
+fn unsigned_num_to_ascii<T: num::Unsigned + std::fmt::Display>(n: T) -> Vec<u8> {
+    n.to_string().as_bytes().to_vec()
+}
+
+// Compare current to next and accumulate counts
+// O(n)
+// RLE for short
+pub fn run_length_encode(cigar: &[u8]) -> Vec<u8> {
+    match cigar {
+        [] => Vec::<u8>::new(),
+        [c] => Vec::from([unsigned_literal_to_u8(1 as u8), *c]),
+        [start_char, the_rest @ ..] => {
+            // more than one value
+            let mut rle = Vec::<u8>::new();
+            let mut current: u8 = *start_char;
+            let mut count: u32 = 1;
+
+            let mut update_rle = |count: u32, c: u8| {
+                rle.extend_from_slice(&unsigned_num_to_ascii(count));
+                rle.extend_from_slice(&[c]);
+            };
+
+            for c in the_rest {
+                if current == *c {
+                    count += 1;
+                } else {
+                    update_rle(count, current);
+
+                    // reset
+                    current = *c;
+                    count = 1;
+                }
+            }
+
+            // last run
+            update_rle(count, current);
+
+            rle
+        }
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
